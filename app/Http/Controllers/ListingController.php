e4ef7549c90a8d921b5369257b014c93bcfd5d67
Anthony Cecconato <antho.cecc@gmail.com>
2022-05-21 15:08:07 +0200

    Add Listing management and authentication

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -49,4 +49,37 @@ class ListingController extends Controller
 
         return redirect('/')->with('message', 'Listing created succcessfully!');
     }
+
+
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    public function update(Request $request, Listing $listing)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'description' => 'required',
+            'tags' => 'required'
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing updated succcessfully!');
+    }
+
+    public function destroy(Listing $listing)
+    {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing deleted successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +20,14 @@ Route::get('/', [ListingController::class, 'index']);
 Route::post('/listings', [ListingController::class, 'store']);
 Route::get('/listings/create', [ListingController::class, 'create']);
 
-Route::get('/listings/{listing}', [ListingController::class, 'show'])
-    ->where('id', '\d+');
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->where('listing', '\d+');
+Route::get('/listings/{listing}', [ListingController::class, 'show'])->where('listing', '\d+');
+
+Route::put('listings/{listing}', [ListingController::class, 'update'])->where('listing', '\d+');
+Route::delete('listings/{listing}', [ListingController::class, 'destroy'])->where('listing', '\d+');
+
+Route::get('/register', [UserController::class, 'create']);
+Route::get('/login', [UserController::class, 'login']);
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+Route::post('/users', [UserController::class, 'store']);
+Route::post('/logout', [UserController::class, 'logout']);
